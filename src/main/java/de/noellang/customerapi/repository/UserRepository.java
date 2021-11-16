47860f7705bf1fff567d4fb46703a9e42db186d5
Noel Lang <noel@lang-technology.de>
2021-11-16 19:35:16 +0100

    Add User repository

---
+++ src/main/java/de/noellang/customerapi/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package de.noellang.customerapi.repository;
+
+import de.noellang.customerapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByEmail(String email);
+
+}
