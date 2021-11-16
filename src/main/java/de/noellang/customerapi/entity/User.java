652a401f78bab8043e503396c929a780d0bab522
Noel Lang <noel@lang-technology.de>
2021-11-16 19:33:40 +0100

    Add User entity

---
+++ src/main/java/de/noellang/customerapi/entity/User.java
@@ -0,0 +1,24 @@
+package de.noellang.customerapi.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+}
