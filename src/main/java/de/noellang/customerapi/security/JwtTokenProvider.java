875646166c4b8146ee0750b36e9f0b9c3a9a0df8
Noel Lang <noel@lang-technology.de>
2021-11-16 19:45:50 +0100

    Add JwtTokenProvider

---
+++ src/main/java/de/noellang/customerapi/security/JwtTokenProvider.java
@@ -0,0 +1,68 @@
+package de.noellang.customerapi.security;
+
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtTokenProvider {
+
+    @Value("${app.jwtSecret}")
+    private String jwtSecret;
+
+    public String generateToken(String userEmail) {
+        Instant now = Instant.now();
+        Instant expiration = now.plus(7, ChronoUnit.DAYS);
+
+        return Jwts.builder()
+                .setSubject(userEmail)
+                .setIssuedAt(Date.from(now))
+                .setExpiration(Date.from(expiration))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+
+    }
+
+    public String generateToken(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        return generateToken(user.getUsername());
+    }
+
+    public String getUserMailFromToken(String token) {
+        Claims claims = Jwts
+                .parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            log.error("Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            log.error("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            log.error("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty.");
+        }
+
+        return false;
+    }
+
+}
