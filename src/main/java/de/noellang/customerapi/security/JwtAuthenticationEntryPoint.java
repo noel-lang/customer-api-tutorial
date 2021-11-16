5d48a1cc62ac7f5fde6d5ae66a24ae918ceed572
Noel Lang <noel@lang-technology.de>
2021-11-16 19:51:03 +0100

    Add JwtAuthenticationFilter

--- src/main/java/de/noellang/customerapi/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/de/noellang/customerapi/security/JwtAuthenticationEntryPoint.java
@@ -1,6 +1,5 @@
 package de.noellang.customerapi.security;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
